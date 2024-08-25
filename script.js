58517dcd4ba9f91793b78dc47a6e218817a393db
Nota Name <email@thisisnotanemail.com>
2024-08-25 13:43:55 -0700

    143

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
       return new bootstrap.Tooltip(tooltipTriggerEl)
-    })
+    });
 });
 
 let videoGroups = {};
